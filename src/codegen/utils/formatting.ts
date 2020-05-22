8f934073a142995a68a6b9558e36bf0b94cc2eb5
Rafael de Melo Silva <rafamsilva@live.com>
2020-05-22 10:45:01 -0300

    handle ivy problem with two points (#73)
    * handle ivy problem with two points
    
    * added null safe check and changed the name for all components

--- src/codegen/utils/formatting.ts
+++ src/codegen/utils/formatting.ts
@@ -24,3 +24,8 @@ export function removeExtraIndentation(code: string, numberOfExtraSpaces: number
     .replace(/^\n/, '')
     .replace(/\s*$/, '')
 }
+
+// function created to make a workaround on ivy problem, which cause queries with two points doesn't work
+export function twoPointsToUnderline(str: string) {
+  return str && str.replace(/[:]/g, '_')
+}
