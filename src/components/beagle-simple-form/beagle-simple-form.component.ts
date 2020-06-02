18d0bbe0dd7ee0209507415b7c6f311911060f9d
Rafael de Melo Silva <rafamsilva@live.com>
2020-06-02 16:13:12 -0300

    BeagleSimpleForm Component (#84)
    * created BeagleSimpleForm Component and associated to default components
    
    * added new endline
    
    * added new endline
    
    * removed ngclass
    
    * - Changed Beagle Button Component to change type.
    - Changed function handleSubmit from Beagle Simple Form Component to prevent reload on submit.
    
    * changed to call onPress function only if the type of the button to be 'button'
    
    * removed breakline

--- src/components/beagle-simple-form/beagle-simple-form.component.ts
+++ src/components/beagle-simple-form/beagle-simple-form.component.ts
@@ -1,4 +1,4 @@
-<!-- 
+/*
   * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,9 +11,22 @@
   * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   * See the License for the specific language governing permissions and
-  * limitations under the License. 
--->
+  * limitations under the License.
+*/
 
-<button [ngClass]="styleId ? '' : 'default-theme'" (click)="handleClick()" [type]="type">
-  {{text}}
-</button>
+import { Component, Input } from '@angular/core'
+import { SimpleFormInterface } from '../schemas/simple-form'
+
+@Component({
+  selector: 'beagle-simple-form',
+  templateUrl: './beagle-simple-form.component.html',
+})
+export class BeagleSimpleFormComponent implements SimpleFormInterface {
+  
+  @Input() onSubmit?: () => void
+
+  handleSubmit(event: Event) {
+    event.preventDefault()
+    this.onSubmit && this.onSubmit()
+  }
+}
