ea109b3e619f1d7106dcf32ab76522f182fb1977
hectorcustodiozup <50678762+hectorcustodiozup@users.noreply.github.com>
2020-11-26 11:26:58 -0300

    Adds platform property (#218)
    * Adds platform property
    
    * Imports omit from beagle web
    
    * fixes Omit import
    
    Co-authored-by: Tiago Peres França <tiagoperesfr@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -21,11 +21,12 @@ import {
   ErrorComponentParams,
   ViewContentManager as CoreViewContentManager,
 } from '@zup-it/beagle-web'
+import { Omit } from '@zup-it/beagle-web/types'
 export { BeagleContext } from '@zup-it/beagle-web/legacy/beagle-context'
 
 export { ErrorComponentParams }
 
-export interface BeagleAngularConfig<Schema> extends BeagleConfig<Schema> {
+export interface BeagleAngularConfig<Schema> extends Omit<BeagleConfig<Schema>, 'platform'> {
   components: {
     'custom:error'?: Type<{} | ErrorComponentParams>,
     'custom:loading'?: Type<{}>,
