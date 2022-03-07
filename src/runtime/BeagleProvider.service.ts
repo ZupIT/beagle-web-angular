c5316ef53dd65ae0a7bfdaa6183f257dfbd41c95
james <jamesson.penha@zup.com.br>
2022-03-07 17:37:59 -0300

    fix platform

--- src/runtime/BeagleProvider.service.ts
+++ src/runtime/BeagleProvider.service.ts
@@ -33,7 +33,7 @@ export class BeagleProvider {
     // @ts-ignore // fixme
     this.service = createCoreBeagleUIService<Schema>({
       ...config,
-      platform: 'WEB',
+      platform: 'WEB-Angular',
       components: {
         ...defaultComponents,
         ...config.components,
